bc287de3936a90661bd5748c87ccddf016775fdb
Aaron Rinn <arinn1250@gmail.com>
2015-10-30 16:36:12 -0700

    made main a little easier to read, removed useless code that was commented out

--- OS/main.c
+++ OS/main.c
@@ -8,19 +8,27 @@
 
 int int80h(); int tinth(); int kbinth();
 
+#define TIMERINT 8
+#define KBINT 9
+
+
+#define SYSCALL 80
+
 
 int procSize = sizeof( PROC );
 
 void setInts() {
 	//SYSCALL
-	set_vec(80, int80h);
+	set_vec(SYSCALL, int80h);
 	
 	lock();
+	
 	//TIMER
-	set_vec(8, tinth);
+	set_vec(TIMERINT, tinth);
 	timer_init();
+
 	//KEYBOARD
-	set_vec(9, kbinth);
+	set_vec(KBINT, kbinth);
 	kbd_init();
 
 	unlock();
@@ -40,21 +48,13 @@ void MTXInit() {
 
 int main() {
 	extern PROC *readyQueue;
-	extern PROC *running;
-	extern PROC proc[NPROC];
-	int i;
-
+	
 	MTXInit();
-	#ifdef _SLEEPER_
-		for(i = 0; i < 5; i++) kfork("/bin/u1");
-	#else
-		kfork("/bin/u1");
-	#endif
+	kfork("/bin/u1");
 
-	printf("P%d is now going to enter infinite loop!\n", running->pid);
 	while(1) {
 		//printf("P%d is now waiting for something in the queue...\n", running->pid);
-		while( readyQueue) tswitch();
+		if( readyQueue ) tswitch();
 		//printf("P%d is now going to switch!\n", running->pid);
 		
 		//printf("P%d is now running\n", running->pid);
